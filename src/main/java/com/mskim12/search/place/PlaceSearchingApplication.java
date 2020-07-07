54e1c755985218064b9894384fc6d7fd14e7fafb
MunSeongKim <chyin370@naver.com>
2020-07-07 16:24:26 +0900

    default

---
+++ src/main/java/com/mskim12/search/place/PlaceSearchingApplication.java
@@ -0,0 +1,13 @@
+package com.mskim12.search.place;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceSearchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceSearchingApplication.class, args);
+	}
+
+}
